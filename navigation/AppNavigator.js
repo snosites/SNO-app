7b3375f61ca4487c4f6f6cadcfa97749c6d04291
Travis Lang <tlang50@icloud.com>
2019-03-22 13:18:47 -0500

    add init nav stack

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createAppContainer, createStackNavigator, createSwitchNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
 
-export default createAppContainer(createSwitchNavigator({
-  // You could add another route here for authentication.
-  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator,
-}));
+import InitScreen from '../screens/InitScreen';
+
+
+const AuthStack = createStackNavigator({ Init: InitScreen });
+
+export default createAppContainer(createSwitchNavigator(
+    {
+        // You could add another route here for authentication.
+        // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+        Main: MainTabNavigator,
+        Auth: AuthStack,
+    },
+    {
+        initialRouteName: 'Auth',
+    }
+));
